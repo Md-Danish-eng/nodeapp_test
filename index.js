10d11c02832cb43521f4d9dc479edeade3ce7f5b
Md-Danish-eng <85988020+Md-Danish-eng@users.noreply.github.com>
2022-02-24 14:41:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello world from Md Danish" }');
+    res.send('{ "response": "Hello world from Danish" }');
 });
 
 app.get('/will', function (req, res) {
